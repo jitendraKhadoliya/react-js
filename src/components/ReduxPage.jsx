27728d597e8f06dd931cb6081c73632643092f3e
jitendra <jitendrasaini352jjnu@gmail.com>
2023-01-26 13:05:03 +0530

    praticing redux from my self

--- src/components/ReduxPage.jsx
+++ src/components/ReduxPage.jsx
@@ -1,35 +1,26 @@
-import React from "react";
-import { useState } from "react";
 import { BiPlusMedical } from "react-icons/bi";
 import { FaMinus } from "react-icons/fa";
 import styled from "styled-components";
-import { useDispatch,useSelector } from "react-redux";
+
 
 const ReduxPage = () => {
-  // const [count, setCount] = useState(0);
 
-  const {count} = useSelector(state => state.custom)
+ 
 
-
-  const dispatch = useDispatch();
-
-
-
-  // inside this page component I am going to access the data from different componet that is stored as store inside the redux folder
   return (
     <>
      <Wrapper>
       <h1 style={{width:"full", textAlign:"center" }} >ReduxPage</h1>
 
-      <div className="container" onClick={()=>{dispatch({type : "INC"})}} >
+      <div className="container" >
         <button >
-        {/* onClick={()=>{dispatch({type : "INC"})}} */}
+        
         <BiPlusMedical className="icon" />
         </button>
-        <h1>{count}</h1>
+        <h1>Row data 0</h1>
         <button >
-        {/* onClick={()=> dispatch({type : "DEC"})} */}
-        <FaMinus className="icon minus_icon" onClick={()=>{dispatch({type : "DEC"})}} />
+        
+        <FaMinus className="icon minus_icon" />
         </button>
       </div>
      </Wrapper>
